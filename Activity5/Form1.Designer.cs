263bf14146e8e5d98d69a3eb9d1d541fe0a39aac
JordanHayen <jordan.hayen@gmail.com>
2021-10-10 14:37:50 -0700

    The files for Activity5

---
+++ Activity5/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Activity5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SelectFile = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TextBoxLowercase = new System.Windows.Forms.TextBox();
+            this.TextBoxFirstWordAlphabetically = new System.Windows.Forms.TextBox();
+            this.TextBoxLastWordAlphabetically = new System.Windows.Forms.TextBox();
+            this.TextBoxLongestWord = new System.Windows.Forms.TextBox();
+            this.TextBoxWordWithMostVowels = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // OpenFileDialog
+            // 
+            this.OpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // SelectFile
+            // 
+            this.SelectFile.Location = new System.Drawing.Point(199, 68);
+            this.SelectFile.Name = "SelectFile";
+            this.SelectFile.Size = new System.Drawing.Size(100, 20);
+            this.SelectFile.TabIndex = 0;
+            this.SelectFile.Text = "Select File";
+            this.SelectFile.UseVisualStyleBackColor = true;
+            this.SelectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(40, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Text In All Lowercase:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(12, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(187, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "First Word Alphabetically:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(187, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Last Word Alphabetically:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(84, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Longest Word:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(20, 218);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(181, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Word With Most Vowels:";
+            // 
+            // TextBoxLowercase
+            // 
+            this.TextBoxLowercase.Location = new System.Drawing.Point(199, 94);
+            this.TextBoxLowercase.Name = "TextBoxLowercase";
+            this.TextBoxLowercase.Size = new System.Drawing.Size(300, 20);
+            this.TextBoxLowercase.TabIndex = 6;
+            // 
+            // TextBoxFirstWordAlphabetically
+            // 
+            this.TextBoxFirstWordAlphabetically.Location = new System.Drawing.Point(199, 128);
+            this.TextBoxFirstWordAlphabetically.Name = "TextBoxFirstWordAlphabetically";
+            this.TextBoxFirstWordAlphabetically.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxFirstWordAlphabetically.TabIndex = 7;
+            // 
+            // TextBoxLastWordAlphabetically
+            // 
+            this.TextBoxLastWordAlphabetically.Location = new System.Drawing.Point(199, 158);
+            this.TextBoxLastWordAlphabetically.Name = "TextBoxLastWordAlphabetically";
+            this.TextBoxLastWordAlphabetically.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxLastWordAlphabetically.TabIndex = 8;
+            // 
+            // TextBoxLongestWord
+            // 
+            this.TextBoxLongestWord.Location = new System.Drawing.Point(199, 188);
+            this.TextBoxLongestWord.Name = "TextBoxLongestWord";
+            this.TextBoxLongestWord.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxLongestWord.TabIndex = 9;
+            // 
+            // TextBoxWordWithMostVowels
+            // 
+            this.TextBoxWordWithMostVowels.Location = new System.Drawing.Point(199, 220);
+            this.TextBoxWordWithMostVowels.Name = "TextBoxWordWithMostVowels";
+            this.TextBoxWordWithMostVowels.Size = new System.Drawing.Size(100, 20);
+            this.TextBoxWordWithMostVowels.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(520, 350);
+            this.Controls.Add(this.TextBoxWordWithMostVowels);
+            this.Controls.Add(this.TextBoxLongestWord);
+            this.Controls.Add(this.TextBoxLastWordAlphabetically);
+            this.Controls.Add(this.TextBoxFirstWordAlphabetically);
+            this.Controls.Add(this.TextBoxLowercase);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SelectFile);
+            this.Name = "Form1";
+            this.Text = "File Reader";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog OpenFileDialog;
+        private System.Windows.Forms.Button SelectFile;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TextBoxLowercase;
+        private System.Windows.Forms.TextBox TextBoxFirstWordAlphabetically;
+        private System.Windows.Forms.TextBox TextBoxLastWordAlphabetically;
+        private System.Windows.Forms.TextBox TextBoxLongestWord;
+        private System.Windows.Forms.TextBox TextBoxWordWithMostVowels;
+    }
+}
+
---
+++ Activity5/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Activity5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            string fileContents = ""; // A string variable that will contain the contents of the inputFile
+            string fileContentsLowercase; // A string variable that will contain the value of the fileContents variable after it has been set to lowercase
+            string fileContentsFirstWordAlphabetically; // A string variable that will contain the first word alphabetically in the fileContents variable
+            string fileContentsLastWordAlphabetically; // A string variable that will contain the last word alphabetically in the fileContents variable
+            string fileContentsLongestWord; // A string variable that will contain the longest word in the the fileContents variable
+            string fileContentsWordWithMostVowels; // A string variable that will contain the word with the most vowels in the fileContents variable
+            StreamReader inputFile; // A StreamReader object to read the contents of the inputFile
+            StreamWriter outputFile; // A StreamWriter object to write the result to an output file
+
+            // The user can select a text file for the program to use using a standard OpenFileDialog. Made with help from the textbook (Gaddis, 308-310, 323)
+            if(OpenFileDialog.ShowDialog() == DialogResult.OK) // The following code runs if a file is chosen using the OpenFileDialog
+            {
+                try
+                {
+                    inputFile = File.OpenText(OpenFileDialog.FileName); // Sets the StreamReader object to the file that the user selected
+
+                    while (!inputFile.EndOfStream) // This loop runs for every line of the file, adding each of them to the fileContents variable. Made with help from the textbook (Gaddis, 316).
+                    {
+                        fileContents += inputFile.ReadLine() + " "; // Sets the fileContents variable to the contents of the chosen file
+                    }
+
+                    inputFile.Close(); // Closes the file
+                } 
+                catch(Exception ex) // If an error occurs while reading the file, an error message is displayed
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+            fileContents = fileContents.Trim(); // Removes the extra space from the end of the fileContents string
+
+            fileContentsLowercase = fileContents.ToLower(); // Sets the fileContentsLowercase variable to the lower case version of the fileContents variable
+            TextBoxLowercase.Text = fileContentsLowercase; // Sets the text of the TextBoxLowercase TextBox to the fileContentsLowercase variable
+
+            fileContentsFirstWordAlphabetically = FindFirstWordAlphabetically(fileContents); // Sets the fileContentsFirstWordAlphabetically variable to the first word alphabetically in the fileContents variable
+            TextBoxFirstWordAlphabetically.Text = fileContentsFirstWordAlphabetically; // Sets the text of the TextBoxFirstWordAlphabetically TextBox to the fileContentsFirstWordAlphabetically variable
+
+            fileContentsLastWordAlphabetically = FindLastWordAlphabetically(fileContents); // Sets the fileContentsLastWordAlphabetically variable to the last word alphabetically in the fileContents variable
+            TextBoxLastWordAlphabetically.Text = fileContentsLastWordAlphabetically; // Sets the text of the TextBoxLastWordAlphabetically TextBox to the fileContentsLasttWordAlphabetically variable
+
+            fileContentsLongestWord = FindLongestWord(fileContents); // Sets the fileContentsLongestWord variable to the longest word in the fileContents variable
+            TextBoxLongestWord.Text = fileContentsLongestWord; // Sets the text of the TextBoxLongestWord TextBox to the fileContentsLongestWord variable
+
+            fileContentsWordWithMostVowels = FindWordWithMostVowels(fileContents); // Sets the fileContentsWordWithMostVowels variable to the word with the most vowels in the fileContents variable
+            TextBoxWordWithMostVowels.Text = fileContentsWordWithMostVowels; // Sets the text of the TextBoxWordWithMostVowels TextBox to the the fileContentsWordWithMostVowels variable
+
+            // The formatted strings are also written to an output file. Made with help from the textbook (Gaddis, 300)
+            try
+            {
+                outputFile = File.CreateText("output.txt"); // A text file called output.txt is created
+
+                outputFile.WriteLine(fileContentsLowercase); // Adds the fileContentsLowercase variable to the file
+                outputFile.WriteLine(fileContentsFirstWordAlphabetically); // Adds the fileContentsFirstWordAlphabetically to the file
+                outputFile.WriteLine(fileContentsLastWordAlphabetically); // Adds the fileContentsLastWordAlphabetically to the file
+                outputFile.WriteLine(fileContentsLongestWord); // Adds the fileContentsLongestWord to the file
+                outputFile.WriteLine(fileContentsWordWithMostVowels); // Adds the fileContentsWordWithMostVowels to the file
+                outputFile.Close(); // Closes the file
+            }
+            catch(Exception ex) // If an exception occurs, an error message is displayed
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+
+        // Takes in a sentence in the form of a string and returns the first word alphabetically
+        private string FindFirstWordAlphabetically(string sentence)
+        {
+            string firstWordAlphabetically; // A string variable that will eventually contain the first word in the sentence alphabetically
+            List<string> words = SentenceToList(sentence); // A list of words in the sentence
+
+            firstWordAlphabetically = words[0]; // Sets the firstWordAlphabetically variable to the first word in the sentence by default
+            for(int i = 1; i < words.Count; i++) // This loop iterates over every word in the sentence except for the first one, as the the firstWordAlphabetically variable is already equal to the first word
+            {
+                firstWordAlphabetically = CompareWords(words[i], firstWordAlphabetically); // Compares the current word with the value of the firstWordAlphabetically variable, sets the firstWordAlphabetically variable to the result
+            }
+
+            return firstWordAlphabetically; // Returns the firstWordAlphabetically variable
+        }
+
+        // Takes in a sentence in the form of a string and returns the last word alphabetically
+        private string FindLastWordAlphabetically(string sentence)
+        {
+            string lastWordAlphabetically; // A string variable that will eventually contain the last word in the sentence alphabetically
+            List<string> words = SentenceToList(sentence); // A list of words in the sentence
+
+            lastWordAlphabetically = words[0]; // Sets the lastWordAlphabetically variable to the first word in the sentence by default
+            for (int i = 1; i < words.Count; i++) // This loop iterates over every word in the sentence except for the first one, as the the lastWordAlphabetically variable is already equal to the first word
+            {
+                if(CompareWords(words[i], lastWordAlphabetically) == lastWordAlphabetically) // Compares the current word with the value of the lastWordAlphabetically variable. If the lastWordAlphabetically variable precedes the current word alphabetically, the lastWordAlphabetically variable is set to the current word
+                {
+                    lastWordAlphabetically = words[i];
+                }
+            }
+
+            return lastWordAlphabetically; // Returns the lastWordAlphabetically variable
+        }
+
+        // Takes in a sentence in the form of a string and returns the longest word in the sentence
+        private string FindLongestWord(string sentence)
+        {
+            string longestWord; // A string variable that will eventually contain the longest word in the sentence
+            List<string> words = SentenceToList(sentence); // A list of words in the sentence
+
+            longestWord = words[0]; // Sets the longestWord variable to the first word in the sentence by default
+            for(int i = 1; i < words.Count; i++) // This loop iterates over every word in the sentence except for the first one, as the the longestWord variable is already equal to the first word
+            {
+                if(words[i].Length > longestWord.Length) // Compares the length of the current word to the length of the longestWord variable. If the current word is longer than the longestWord variable, the longestWord variable is set to the current word
+                {
+                    longestWord = words[i];
+                }
+            }
+
+            return longestWord; // Returns the longestWord variable
+        }
+
+        // Takes in a sentence in the form of a string and returns the word in the sentence with the most vowels
+        private string FindWordWithMostVowels(string sentence)
+        {
+            string wordWithMostVowels; // A string variable that will eventually contain the word with the most vowels
+            List<string> words = SentenceToList(sentence); // A list of words in the sentence
+
+            wordWithMostVowels = words[0]; // Sets the wordWithMostVowels variable to the first word in the sentence by default
+            for (int i = 0; i < words.Count; i++) // This loop iterates over every word in the sentence except for the first one, as the the wordWithMostVowels variable is already equal to the first word
+            {
+                if(VowelsInWord(words[i]) > VowelsInWord(wordWithMostVowels)) // Compares the number of vowels in the current word with that of the wordWithMostVowels vairalbe. If the current word contains more vowels than the wordWithMostVowels variable, the wordWithMostVowels variable is set to the current word
+                {
+                    wordWithMostVowels = words[i];
+                }
+            }
+
+            return wordWithMostVowels; // Returns the wordWithMostVowels variable
+        }
+
+        // Takes in a word in the form of a string and returns the number of vowels in that word
+        private int VowelsInWord(string word)
+        {
+            int vowels = 0; // An integer variable that will contain the number of vowels in the word
+
+            for(int i = 0; i < word.Length; i++) // This loop iterates over every letter in the word
+            {
+                if (word[i] == 'a' || word[i] == 'e' || word[i] == 'i' || word[i] == 'o' || word[i] == 'u') // If the current letter is a vowel, the vowels variable is incremented
+                {
+                    vowels++;
+                }
+            }
+
+            return vowels; // Returns the vowels variable
+        }
+
+        // Takes in a sentence as a string and returns a list of strings representing the words in that sentence
+        private List<string> SentenceToList(string sentence)
+        {
+            List<string> words = new List<string>(); // A list of strings that will eventually contain the words in the sentence
+
+            words.Add(""); // Adds an item to the words list as it will need to contain at least one item
+            for (int i = 0, j = 0; i < sentence.Length; i++) // This loop iterates over character in the sentence. It has two counter variables, one to track the current character in the sentence and another to keep track of the current item in the words list
+            {
+                if (sentence[i] != ' ') // If the current character is not a space, it is added to the current item of the words list. It is set to lower case as strings are easier to compare when they are in the same case
+                {
+                    words[j] += sentence[i].ToString().ToLower();
+                }
+                else // If the current character is a space, then a new item is added to the words list and the counter variable tracking the current item in the words list is incremented
+                {
+                    j++;
+                    words.Add("");
+                }
+            }
+
+            return words; // Returns the words list
+        }
+
+        // This method takes in two strings and returns whichever is first alphabetically
+        private string CompareWords(string word1, string word2)
+        {
+            string alphabet = "abcdefghijklmnopqrstuvwxyz"; // A string that contains the alphabet
+
+            for(int i = 0; i < word1.Length; i++) // This loop iterates over the two words one character at a time, comparing them to see which is first alphabetically
+            {
+                if (alphabet.IndexOf(word1[i]) < alphabet.IndexOf(word2[i])) // If the current character in word1 appears earlier in the alphabet than the current character in word2 then word1 is returned
+                {
+                    return word1;
+                }
+                else if(alphabet.IndexOf(word1[i]) > alphabet.IndexOf(word2[i])) // If the current character in word2 appears earlier in the alphabet than the current character in word1 then word2 is returned
+                {
+                    return word2;
+                }
+            }
+
+            return word1; // If the program reaches this point then that means that the two words are identical, so it doesn't matter which is returned
+
+        }
+
+    }
+}
+
+// References
+// Gaddis, T. (2020). Starting Out With Visual C#. Pearson.
